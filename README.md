642b33c64ace366728b9071eeb2245782950e4f1
Jeremy Fryd and Sara Jensen <jeremy@>
2016-08-17 13:09:35 -0700

    update README.md

---
+++ README.md
@@ -0,0 +1,36 @@
+# _leap year checker_
+
+#### _Were you born in a leap year?, Aug 17, 2016_
+
+#### By _**Sara Jensen and Jeremy Fryd**_
+
+## Description
+
+* The program should return when a year is not a leap year.
+Input Example: 1993
+Output Example: false
+* The program should return 'true' for years divisible by 4, since those are leap years.
+Input Example: 2004
+Output Example: true
+The program should return 'false' for years divisible by 100, since those are not leap years.
+* Input Example: 1900
+Output Example: false
+* The program should return 'true' for years divisible by 400, since those are leap years.
+Input Example: 2000
+Output Example: true
+
+## Known Bugs
+
+_No known bugs._
+
+## Support and contact details
+
+_If you run into any issues or have questions, ideas or concerns, contact us at jensen.sara.e@gmail.com._
+
+## Technologies Used
+
+_Javascript, Bootstrap, jquery, CSS, HTML_
+
+### License
+
+Copyright (c) 2016 **_MIT License_**
--- js/scripts.js
+++ js/scripts.js
@@ -7,17 +7,19 @@ $(document).ready(function() {
   $("form#yearForm").submit(function(event) {
     event.preventDefault();
     var yearInputted = parseInt($("#yearInput").val());
-    if (yearInputted % 100 === 0 && yearInputted % 400 === 0) {
-      alert(true);
-    } else if
-      (yearInputted % 100 === 0) {
-      alert(false);
-    } else if
-      (yearInputted % 4 === 0 || yearInputted % 400 === 0) {
-      alert(true);
-    } else if
-      (yearInputted % 4 != 0) {
-      alert(false);
-    };
+    // if (yearInputted % 100 === 0 && yearInputted % 400 === 0) {
+    //   alert(true);
+    // } else if
+    //   (yearInputted % 100 === 0 || yearInputted % 4 != 0) {
+    //   alert(false);
+    // } else if
+    //   (yearInputted % 4 === 0 || yearInputted % 400 === 0) {
+    //   alert(true);
+
+      if ((yearInputted % 100 === 0 && yearInputted % 400 != 0) || yearInputted % 4 != 0) {
+        alert(false);
+      } else {
+        alert(true);
+      }
   });
 });
